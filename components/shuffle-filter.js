953f2dc7bc24e5bff8a37f423800f8ccfd8e920a
igor_ivanov <igorpi25@gmail.com>
2023-08-01 15:03:26 +0900

    decorate scheme

--- components/shuffle-filter.js
+++ components/shuffle-filter.js
@@ -10,8 +10,12 @@ Vue.component('shuffle-filter', {
     },
     props: ['id'],
     template: `
-    <div @focus="onFocus" tabindex="-1" class="flex h-16 w-16 justify-center items-center bg-gray-100 outline-indigo-600 outline-1 hover:outline-dashed focus:outline focus:outline-2">
-        {{id}}
+    <div class="flex items-center">
+        <div class="flex-none text-gray-400">-</div>
+        <div @focus="onFocus" tabindex="-1" class="flex h-16 w-16 justify-center items-center bg-gray-100 outline-indigo-600 outline-1 hover:outline-dashed focus:outline focus:outline-2">
+            <span class="text-gray-600">{{id}}</span>
+        </div>
+        <div class="flex-none text-gray-400">-</div>
     </div>
     `,
 });
