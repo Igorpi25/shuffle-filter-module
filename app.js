d86847f760a09609befe49cc16ec61224344b83e
igor_ivanov <igorpi25@gmail.com>
2023-08-01 14:41:36 +0900

    add local and incoming values, visual decorate

--- app.js
+++ app.js
@@ -1,17 +1,116 @@
 var app = new Vue({
     el: '#app',
-    data() {
-        return {
-            runSchemeResult: '(runSchemeResult)',
-            message: '(nothing selected)',
-            selectedScheme: {},
-            selectedItem: {},
-        };
+    data:
+    {
+        runSchemeResult: '(runSchemeResult)',
+        message: '(nothing selected)',
+        selectedItem: Object,
+        scheme: {
+            id: 'Main',
+            type: 'row',
+            childs: [
+                {
+                    id: 'D',
+                    type: 'column',
+                    childs: [
+                        {
+                            id: 'A',
+                            type: 'row',
+                            childs: [
+                                {
+                                    id: 'A1',
+                                    type: 'filter',
+                                    result: false,
+                                    params: [
+                                        {
+                                            name: 'Alpha',
+                                            type: 'set',
+                                            source: 'inline',
+                                            value: ['cafe', 'kids'],
+                                        },
+                                        {
+                                            name: 'Beta',
+                                            type: 'set',
+                                            source: 'local',
+                                            key: "ballooning-fest",
+                                        }
+                                    ]
+                                },
+                                {
+                                    id: 'A2',
+                                    type: 'filter',
+                                    result: true,
+                                    params: [
+                                        {
+                                            name: 'User',
+                                            type: 'set',
+                                            source: 'incoming',
+                                            key: 'user',
+                                        },
+                                        {
+                                            name: 'Cafe',
+                                            type: 'set',
+                                            source: 'incoming',
+                                            key: 'place',
+                                        }
+                                    ]
+                                },
+                            ],
+                        },
+                        {
+                            id: 'B',
+                            type: 'column',
+                            childs: [
+                                {
+                                    id: 'B1',
+                                    type: 'filter',
+                                    result: true,
+                                },
+                                {
+                                    id: 'B2',
+                                    type: 'filter',
+                                    result: false,
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    id: 'C1',
+                    type: 'filter',
+                    result: true,
+                },
+                {
+                    id: 'Z',
+                    type: 'column',
+                    childs: [
+                        {
+                            id: 'Z1',
+                            type: 'filter',
+                            result: false,
+                        },
+                        {
+                            id: 'Z2',
+                            type: 'filter',
+                            result: true,
+                        },
+                    ],
+                },
+            ],
+        },
+        local: {
+            'ballooning-fest': ['sunday', 'beach'],
+            'hollydays': ['sunday', 'saturday'],
+        },
+        incoming: {
+            'user': ['cafe', 'kids'],
+            'place': ['cafe', 'coffe', 'tea', 'chicken', 'pizza', 'latte', 'free-hot-water'],
+        },
     },
     computed: {
         varsOfSchemes() {
             return chains || []
-        },
+        }
     },
     methods: {
         onSelectItem(item) {
