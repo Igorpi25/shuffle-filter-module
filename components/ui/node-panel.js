b994b648145184fbd71c44664a11cd91fe50db37
Jack Blackjack <chugarev@gmail.com>
2023-08-06 22:55:50 +0300

    add: add blink then move node and edit inline

--- components/ui/node-panel.js
+++ components/ui/node-panel.js
@@ -2,12 +2,28 @@ Vue.component('node-panel', {
     props: ['local', 'incoming', 'schema', 'selectedItem'],
     data() {
         return {
+            inlineParamValue: '',
         };
     },
     computed: {
         serializedSelectedItem() {
             return JSON.stringify(this.selectedItem);
         }
+    },
+    watch: {
+        selectedItem: {
+            immediate: true,
+            handler(newSelectedItem) {
+                if (newSelectedItem && newSelectedItem.params) {
+                    const inlineParam = newSelectedItem.params.find((param) => param.source === 'inline');
+                    if (inlineParam) {
+                        this.inlineParamValue = inlineParam.value;
+                    }
+                } else {
+                    this.inlineParamValue = null;
+                }
+            },
+        },
     },
     methods: {
         selectElementById(id) {
@@ -28,6 +44,19 @@ Vue.component('node-panel', {
         handlerOnMoveItem: function ({ direction, id }) {
             const parent = this.selectParentById(id)
 
+            const blinkMeAndFocus = () => {
+                this.$nextTick(() => {
+                    const selectedItemElement = document.getElementById(this.selectedItem.id);
+                    if (selectedItemElement) {
+                        selectedItemElement.classList.add('blink-animation');
+                        setTimeout(() => {
+                            selectedItemElement.classList.remove('blink-animation');
+                            selectedItemElement.focus();
+                        }, 5000);
+                    }
+                });
+            }
+
             if (parent) {
                 const index = this.selectedItem.id
                     ? parent.childs.findIndex((child) => child.id === this.selectedItem.id)
@@ -37,19 +66,15 @@ Vue.component('node-panel', {
                     if (direction === "forward" && index < parent.childs.length - 1) {
                         parent.childs.splice(index, 1);
                         parent.childs.splice(index + 1, 0, this.selectedItem);
+
+                        blinkMeAndFocus();
                     } else if (direction === "backward" && index > 0) {
                         parent.childs.splice(index, 1);
                         parent.childs.splice(index - 1, 0, this.selectedItem);
+
+                        blinkMeAndFocus();
                     }
                 }
-
-                // Назначение стиля :focus на элемент с выбранным id
-                this.$nextTick(() => {
-                    const selectedItemElement = document.getElementById(this.selectedItem.id);
-                    if (selectedItemElement) {
-                        selectedItemElement.focus();
-                    }
-                });
             }
         },
         handlerOnRemoveItem: function (id) {
@@ -62,6 +87,19 @@ Vue.component('node-panel', {
 
                 if (-1 !== index) {
                     parent.childs.splice(index, 1);
+                }
+            }
+        },
+        saveInlineParamValue() {
+            if (this.selectedItem && this.selectedItem.params) {
+                const inlineParam = this.selectedItem.params.find((param) => param.source === 'inline');
+                try {
+                    const newValueAsArr = String(this.inlineParamValue).split(',').map(v => String(v).trim()).filter(v => v !== undefined);
+                    if (inlineParam && Array.isArray(newValueAsArr)) {
+                        inlineParam.value = newValueAsArr;
+                    }
+                } catch (error) {
+                    throw new Error(`Ошибка при обновлении значения свойства!`)
                 }
             }
         },
@@ -113,29 +151,25 @@ Vue.component('node-panel', {
         <!-- Params -->
         <div v-for="param in selectedItem.params" :key="param.name">
             <div class="flex bg-gray-200 p-2 rounded-b-lg">
-                <h2 class="text-bold">Param: {{param.name}}</h2>
+                <h2 class="text-bold">Param: {{ param.name }}</h2>
             </div>
+
             <div class="flex flex-col p-2">
-                <div>
-                    name: {{ param.name }}
+                <div>name: {{ param.name }}</div>
+                <div>type: {{ param.type }}</div>
+                <div>source: {{ param.source }}</div>
+
+                <div v-if="param.key">key: {{ param.key }}</div>
+
+                <div v-if="param.source == 'inline'">
+                    <input v-model="inlineParamValue" @keydown.enter="saveInlineParamValue" />
+                    <br /><span class="italic text-sm">(укажите значения свойства через запятую, чтобы сохранить новое значение - нажмите Enter)</span>
                 </div>
-                <div>
-                    type: {{ param.type }}
+                <div v-else-if="param.source == 'local'" class="text-gray-400">
+                    local-value: {{ local[param.key] }}
                 </div>
-                <div>
-                    source: {{param.source}}
-                </div>
-                <div v-if="param.key">
-                    key: {{param.key}}
-                </div>
-                <div v-if="param.source=='inline'">
-                    inline-value: {{param.value}}
-                </div>
-                <div v-if="param.source=='local'" class="text-gray-400">
-                    local-value: {{local[param.key]}}
-                </div>
-                <div v-if="param.source=='incoming'" class="text-gray-400">
-                    incoming-value: {{incoming[param.key]}}
+                <div v-else-if="param.source == 'incoming'" class="text-gray-400">
+                    incoming-value: {{ incoming[param.key] }}
                 </div>
             </div>
         </div>
@@ -174,4 +208,3 @@ Vue.component('node-panel', {
     </div>
     `,
 });
-
