9a5eba8f7d3bf662534ec7ee1a72330e5c64fec7
igor_ivanov <igorpi25@gmail.com>
2023-08-15 13:53:29 +0900

    removed TopPanel from index.html and selectedScheme from app.js

--- app.js
+++ app.js
@@ -4,39 +4,17 @@ var app = new Vue({
     {
         runSchemeResult: '(runSchemeResult)',
         message: '(nothing selected)',
-        selectedScheme: chains[0].value,
+        scheme: chains[0].value,
         selectedItem: {},
     },
     computed: {
-        varsOfSchemes() {
-            return chains || []
-        },
-        serializedLocal() {
-            return 'params' in this.selectedScheme ? JSON.stringify(this.selectedScheme.params?.local || {}) : {}
-        },
-        serializedIncoming() {
-            return 'incoming' in this.selectedScheme ? JSON.stringify(this.selectedScheme.params?.incoming || {}) : {}
-        },
         selectedItemIsNotEmpty() {
             return Object.keys(this.selectedItem || {}).length
         },
-        appSchemeResult() {
-            return runScheme(this.selectedScheme);
-        }
     },
     methods: {
         onSelectItem(item) {
             this.selectedItem = item;
         },
-        getSchemeResult() {
-            if (Object.keys(this.selectedScheme || {}).length) {
-                return runScheme(this.selectedScheme);
-            } else {
-                return 'false';
-            }
-        },
-        onChangeSelectedScheme() {
-            this.selectedItem = {}
-        }
     },
 });
