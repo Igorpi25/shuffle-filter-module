574f8485e0d0baa0ac32390d43c37a471b6a9198
igor_ivanov <igorpi25@gmail.com>
2023-08-29 14:13:06 +0900

    inject param to scheme

--- endpoints/active-tiger/page.js
+++ endpoints/active-tiger/page.js
@@ -4,13 +4,19 @@ var page = new Vue({
     data:
     {
         scheme: chains[1].value,
-
     },
     methods: {
         getUrlParams() {
             return this.$route.query;
         },
+        injectParamsToScheme() {
+            let params = this.getUrlParams();
+
+            this.scheme.params.incoming.user = params.user;
+            this.scheme.params.incoming.place = params.place;
+        },
         getSchemeResult() {
+            this.injectParamsToScheme();
             return runScheme(this.scheme);
         }
     },
