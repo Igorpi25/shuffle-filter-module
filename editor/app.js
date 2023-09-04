2ac5ad589042828dd905edf00c551e4493886683
igor_ivanov <igorpi25@gmail.com>
2023-09-05 02:37:30 +0900

    add menu, save single scheme

--- editor/app.js
+++ editor/app.js
@@ -22,6 +22,36 @@ var app = new Vue({
         getUrlParams() {
             return this.$route.query;
         },
+        saveSchemeInLocaleStorage() {
+            this.scheme.savedAt = Date();
+            const parsed = JSON.stringify(this.scheme);
+            localStorage.setItem('scheme', parsed);
+        },
+        loadSchemeFromLocaleStorage() {
+            if (localStorage.getItem('scheme')) {
+                try {
+                  this.scheme = JSON.parse(localStorage.getItem('scheme'));
+                } catch(e) {
+                  //localStorage.removeItem('scheme');
+                  console.log("loadSchemeFromLocaleStorage e="+e);
+                }
+              }
+        },
+        getSavedAt() {
+            if (localStorage.getItem('scheme')) {
+                try {
+                  let savedScheme = JSON.parse(localStorage.getItem('scheme'));
+                  return savedScheme.savedAt;
+                } catch(e) {
+                  //localStorage.removeItem('scheme');
+                  console.log("loadSchemeFromLocaleStorage e="+e);
+                }
+            } else {
+                return '(no saved scheme)';
+            }
+
+        }
+        
     },
     beforeMount() {
         if(this.$route.query.index === undefined) {
