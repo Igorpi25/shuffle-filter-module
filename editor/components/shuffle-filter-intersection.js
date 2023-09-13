32d122c1088fe421328c90b7da030d4439e2f8d6
igor_ivanov <igorpi25@gmail.com>
2023-09-13 23:41:44 +0300

    update filter-intersection truncate id-text

--- editor/components/shuffle-filter-intersection.js
+++ editor/components/shuffle-filter-intersection.js
@@ -12,7 +12,7 @@ Vue.component('shuffle-filter-intersection', {
     <div class="flex items-center" :title="id">
         <div class="flex-none text-gray-400">-</div>
         <div @focus="onFocus" tabindex="-1" class="flex h-16 w-16 p-2 justify-center items-center bg-stone-300 outline-indigo-600 outline-1 hover:outline-dashed focus:outline focus:outline-2">
-            <span class="text-gray-600 text-center text-ellipsis text-xs">{{id}}</span>
+            <span class="text-gray-600 text-center truncate overflow-hidden text-xs">{{id}}</span>
         </div>
         <div class="flex-none text-gray-400">-</div>
     </div>
